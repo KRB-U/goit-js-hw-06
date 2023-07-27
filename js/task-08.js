1a8c76adcba54342f41660fb6158d93e010b1ead
unknown <rubikonspace@ukr.net>
2023-07-27 18:59:43 +0300

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
+// console.dir(form);
+form.addEventListener("submit", handlerMangmntForm);
 
+function handlerMangmntForm(evt) {
+  evt.preventDefault();
+
+  //   const { email, password } = evt.currentTarget.elements;
+
+  const {
+    elements: { email, password },
+  } = evt.currentTarget;
+
+  if (
+    evt.currentTarget.email.value !== "" &&
+    evt.currentTarget.password.value !== ""
+  ) {
+    console.log(email.value, password.value);
+  } else {
+    alert("всі поля повинні бути заповнені");
+  }
+}
