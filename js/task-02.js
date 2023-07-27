e3ad7dc3e48bbe869e047d37ff5cea4ff12f773d
unknown <rubikonspace@ukr.net>
2023-07-27 14:23:59 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,14 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.createElement("li");
+list.textContent = ingredients["Potatoes"];
+console.log(list.textContent);
+list.classList.add("item");
+list.innerHTML;
