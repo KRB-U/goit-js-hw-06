72626342811f02c77bac305a9176b71c67f67fe1
unknown <rubikonspace@ukr.net>
2023-07-27 13:14:25 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,17 @@
-const count = document.querySelector("categories");
-count.map();
+const list = document.querySelectorAll("#categories");
+const listNum = list.length;
+
+console.log(list);
+console.log(listNum);
+
+// count.map();
+// `Number of categories: ${}`
+
+// `Category: ${h2}` // firstElementChild
+// `Elements: ${}`
+
+// `Category: ${h2}`
+// `Elements: ${}`
+
+// `Category: ${h2}`
+// `Elements: ${}`
