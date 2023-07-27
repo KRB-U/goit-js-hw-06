974af8d43bc090dae4c318aec6211f83306c3821
unknown <rubikonspace@ukr.net>
2023-07-27 19:34:19 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+// const body = documnet.body;
+const putColor = document.querySelector("color");
+const chgColor = Document.querySelector("change-color");
+
+function handlerChangeColor() {
+  // style.getRandomHexColor();
+}
+
+chgColor.addEventListener("click", handlerChangeColor);
+
+// putColor.addEventListener("click", handlerChangeColor);
