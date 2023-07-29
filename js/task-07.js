9e795a99c646108e8a27ba05f44329f08fba4376
unknown <rubikonspace@ukr.net>
2023-07-29 08:37:37 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,6 @@
 const inpArea = document.querySelector("#font-size-control");
 
-// console.dir(inpArea);
+console.dir(inpArea);
 
 const spn = document.querySelector("#text");
 
@@ -9,7 +9,7 @@ const spn = document.querySelector("#text");
 inpArea.addEventListener("input", changeSize);
 
 function changeSize(evt) {
-  spn.style.fontSize = evt.currentTarget.value + "px";
+  spn.style.fontSize = evt.target.value + "px";
 
   // console.dir(evt.currentTarget);
 }
